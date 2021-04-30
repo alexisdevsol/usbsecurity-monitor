ee833a243228e0b826705b665e32a56ced124d65
dev <alexis89.dev@gmail.com>
2021-04-30 19:16:00 -0400

    Incrementada version por problemas con pypi

--- usbsecurity_monitor/__version__.py
+++ usbsecurity_monitor/__version__.py
@@ -1,7 +1,7 @@
 __title__ = 'usbsecurity-monitor'
 __description__ = 'usbsecurity-monitor is the program to control USB ports'
 __url__ = 'https://pypi.org/project/usbsecurity-monitor/'
-__version__ = '2.0.4'
+__version__ = '2.1.0'
 __author__ = 'Alexis Torres Valdes'
 __author_email__ = 'alexis89.dev@gmail.com'
 __credits__ = ['Alexis Torres Valdes']

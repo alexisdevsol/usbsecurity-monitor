f012296055649340e459528bb6b3cd09c8042074
dev <alexis89.dev@gmail.com>
2021-05-10 13:50:57 -0400

    Nueva rama 'develop'

--- usbsecurity_monitor/__version__.py
+++ usbsecurity_monitor/__version__.py
@@ -21,11 +21,11 @@
 __title__ = 'usbsecurity-monitor'
 __description__ = 'usbsecurity-monitor is the program to control USB ports'
 __url__ = 'https://pypi.org/project/usbsecurity-monitor/'
-__version__ = '4.1.0'
+__version__ = '5.0.0'
 __author__ = 'Alexis Torres Valdes'
 __author_email__ = 'alexis89.dev@gmail.com'
 __credits__ = ['Alexis Torres Valdes']
 __maintainer__ = 'Alexis Torres Valdes'
 __license__ = 'LGPL'
 __copyright__ = 'Copyright 2021 Alexis Torres Valdes'
-__status__ = 'Production'
+__status__ = 'Development'
